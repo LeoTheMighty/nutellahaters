34b2af29f096abed602b6bfa987891d5bae25fb9
Leo <leonid@ac93.org>
2018-04-19 02:04:49 -0400

    add cache files

---
+++ Cache.c
@@ -0,0 +1,19 @@
+/*
+ * Cache.c
+ *
+ *  Created on: Apr 19, 2018
+ *      Author: student
+ */
+
+struct CacheLine {
+
+};
+
+struct CacheSet {
+
+};
+
+struct Cache {
+
+};
+
---
+++ Cache.h
@@ -0,0 +1,15 @@
+/*
+ * Cache.h
+ *
+ *  Created on: Apr 19, 2018
+ *      Author: lbelyi
+ */
+
+#ifndef CACHE_H_
+#define CACHE_H_
+
+struct CacheLine;
+struct CacheSet;
+struct Cache;
+
+#endif /* CACHE_H_ */
