591fa189ceb67d6b28596483312753d18f5c92e6
Leonid Belyi <leonid@ac93.org>
2018-04-25 12:11:38 -0400

    create add_to_cache function

--- csim.c
+++ csim.c
@@ -9,19 +9,28 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+
+unsigned int hits = 0;
+unsigned int misses = 0;
+unsigned int evictions = 0;
+unsigned int time = 0;
 
 struct CacheLine {
 	unsigned long tag; // 8 bytes?
 	uint8_t is_valid;   // determines if it's been set
+	unsigned int timeAdded;
 	//uint8_t *data;     // we actually probably don't need this
 };
 
 struct CacheSet {
 	struct CacheLine *lines; // must malloc()
+	unsigned int numLines;
 };
 
 struct Cache {
 	struct CacheSet *sets; // must malloc()
+	unsigned int numSets;
 	unsigned int E; // associativity
 	unsigned int b; // # of bits in offset
 	unsigned int s; // # of bits for set (index)
@@ -38,6 +47,7 @@ struct Cache* initCache(unsigned int E, unsigned int b, unsigned int s) {
 	cache->b = b;
 	cache->s = s;
 	unsigned int numSets = pw2(s);
+	cache->numSets = numSets;
 	cache->sets = malloc(numSets * sizeof(struct CacheSet));
 	for (int i = 0; i < numSets; i++) {
 		// init each set
@@ -53,6 +63,7 @@ struct CacheSet initSet(unsigned int E) {
 	struct CacheSet set;
 	unsigned int numLines = pw2(E);
 	set.lines = malloc(numLines * sizeof(struct CacheLine));
+	set.numLines = numLines;
 	for (int i = 0; i < numLines; i++) {
 		set.lines[i] = initLine();
 	}
@@ -75,22 +86,74 @@ struct CacheLine initLine() {
  * @return 2^p
  */
 unsigned int pw2(unsigned int p) {
-	unsigned int pow = 1;
-	for (; p > 0; p--) {
-		pow *= 2;
-	}
-	return pow;
+	return (1 << p);
 }
 
+/** Returns the char of the result from the adding
+ *
+ */
+char add_to_cache(Cache *cache, unsigned int tag, unsigned int index) {
+	// Use the index to get where ya gotta go, then use the tag to verify
+	CacheSet *set = cache->sets + index;
 
-void clear_cache(cache *cache, long int numSets, int numLines, long  int blockSize){
+	unsigned firstEmptyIndex = -1;
+	for (int i = 0; i < set->numLines; i++) {
+		CacheLine *line = set->lines + i;
+		// Check each tag in the set
+		if (line->tag == tag && line->isValid == 1) {
+			// Then it's a hit
+			line->timeAdded = time++;
+			return "h";
+		}
+
+		if (firstEmptyIndex == -1 && line->isValid = 0) {
+			firstEmptyIndex = i;
+		}
+	}
+
+	// If it reaches here, then it's a miss
+	if (firstEmptyIndex != -1) {
+		// Then no eviction necessary, just a miss
+		CacheLine *emptyLine = set->lines + firstEmptyIndex;
+		emptyLine->tag = tag;
+		emptyLine->isValid = 1;
+		emptyLine->timeAdded = time++;
+		return "m";
+	}
+
+	// Now it's an eviction, decide whom'st'd've to evict
+
+	// Then ya gotta deal with the gucci eviction stuff
+	// LRU (least recently used)
+	unsigned int lowestTime = INT_MAX;
+	CacheLine *lowestLine = NULL;
+	for (int i = 0; i < cache->numSets; i++) {
+		CacheSet *set = cache->sets + i;
+		for (int j = 0; j < set->numLines; j++) {
+			CacheLine *line = set->lines + j;
+			unsigned timeAdded = line->timeAdded;
+			if (timeAdded < lowestTime) {
+				// Then this is the new lowest time
+				lowestTime = timeAdded;
+				lowestLine = line;
+			}
+		}
+	}
+
+	// Then you evict the lowestLine
+	lowestLine->tag = tag;
+	lowestLine->timeAdded = time++;
+	return "e";
+}
+
+void clear_cache(Cache *cache, long int numSets, int numLines, long  int blockSize){
 	int setIndex;
 	for (setIndex = 0; setIndex < numSets; setIndex ++){
 		cacheSet set = cache.sets[setIndex];
-		if (set.lines != NULL){	
+		if (set.lines != NULL){
 			free(set.lines);
 		}
-	} 
+	}
 	if (Cache.sets != NULL) {
 		free(cache.sets);
 	}
@@ -168,12 +231,29 @@ int main(int argc, char* argv[])
 
 		int hexNum = strtol(hex, NULL, 16);
 
+		// I don't even know if we're using s and b lmao
+		unsigned tag = hexNum >> (s + b); // TODO CHECK THIS
+		unsigned index = (hexNum << (64 - (s + b))) >> (64 - s); // TODO check this :p
+
 		//from i+1 until the end, that string represents the size
-		i++;
-		int dataSize = atoi(&line[i]);
+		//i++;
+		//int dataSize = atoi(&line[i]);
 		// Ends at the endline anyways, so will be the string
 
 		// done reading from the line, now process it
+		// Process the hex
+		char result = add_to_cache(cache, tag, index);
+		switch (result) {
+			case 'h':
+				hits++;
+				break;
+			case 'm':
+				misses++;
+				break;
+			case 'e':
+				evictions++;
+				break;
+		}
 	}
 
 	fclose(fp);
@@ -184,4 +264,3 @@ int main(int argc, char* argv[])
 
     return EXIT_SUCCESS;
 }
-
