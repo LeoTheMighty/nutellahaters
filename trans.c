575fb83db80acf10d8717a6c6782ce794caf30df
ncarvajalerker <36217235+ncarvajalerker@users.noreply.github.com>
2018-04-30 16:14:21 -0400

    Update trans.c
    final with comments

--- trans.c
+++ trans.c
@@ -39,27 +39,21 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
   if (N == 32 && M == 32) {
     b_s = 8;
 
-////2 outer loops iterates accross blocks / 2 inner loops iterate through each block. 
+    //2 loops iterates through blocks and 2 loops iterate across each block. 
     for (j_block = 0; j_block < (N / b_s); j_block++) {
       for (i_block = 0; i_block < (M / b_s); i_block++) {
         for (i = (i_block * b_s); i < ((i_block + 1) * b_s); i++) {
           for(j = (j_block * b_s); j < ((j_block + 1) * b_s); j++) {
-            if(i != j){  //When row and collumn is not equal then we can change the value in B to the desired value in A
+            if(i != j){  //When row and collumn is not equal then we can change the value in B array to the value in A
               B[j][i] = A[i][j];
             } else {
-              //However, when row == column, it means we touched the diagonal 
-              //of the square. Now, we are accessing A and B in the same innerloop,
-              // and A, B have differnt tags. For the diagonal blocks, there are 
-              //conflicts when accessing the same row of A and B. This part should 
-              //not be moved or reassigned to avoid misses. Instead, we used 2 temporary 
-              //variable, 1 to store the position, 1 to store the value for later assign. 
-              // use temporary local variables to avoid having to re-access those elements
+              //When a row == column it touched the diagonal of the matrix so use 2 temporary vars to stop and store the position
               temp = A[i][j];
               d = i;
             }
           }
 
-           // Each traverse only has 1 element in the diagonal line. So we will assign it here, outside of the loop.
+           // Each traverse only has 1 element in the diagonal line that is assigned outside of the loop.
           if (i_block == j_block) {
             B[d][d] = temp;
           }
@@ -74,10 +68,7 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
     // two loops to go through each row and column block 
     for(colRun=0; colRun<64; colRun+=8 ){
         for(rowRun=0; rowRun<64; rowRun+=8 ){
-
-        /* 
-        The first loop is for a 4 collumn 8 row of A. In this loop we  use the supporting variables to store all elements of a row. Then we try to transpose with the right positions in B. However, we are doing on a 4row x 8 collumn B to avoid cache miss so not all elements will be transposed correctly. For example A[3][5] cannot be transposed to B[5][3]. Thus, elements which can't be transposed correctly will be stored in another location for later use.
-        */ 
+        //The first loop is for a 4 collumn 8 row of A. use loop to store variables. the nchange to the poistion of B. Not everything will be transposed correct, there will be some misses.
           for(k=0; k<4; k++){ 
             a0 = A[colRun+k][rowRun+0];
             a1 = A[colRun+k][rowRun+1];
@@ -97,13 +88,7 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
             B[rowRun+3][colRun+k+0] = a3; 
             B[rowRun+3][colRun+k+4] = a4; 
           }
-
-            /* Part B, moving sub-matrix b to sub-matrix c
-             * and moving A->B for sub-matrix b and move matrix d
-             */
-            /*
-            Now that we have dealt with the first 4 col 8 arrow of A. The next job to deal with the "later use" assignment above. The "later use" assignments that we did above have taken a lot of places, so we need to bring these elements to their right positions. 
-             */
+          //This is for the stored variables from the loop above that are correct and puts them in the right position. moves one matrix to another matrix b.
           a0 = A[colRun+4][rowRun+4];
           a1 = A[colRun+5][rowRun+4];
           a2 = A[colRun+6][rowRun+4];
@@ -113,8 +98,8 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
           a6 = A[colRun+6][rowRun+3];
           a7 = A[colRun+7][rowRun+3];
 
-          B[rowRun+4][colRun+0] = B[rowRun+3][colRun+4];  // B[4][0] = a4 = A[0][4] For example
-          B[rowRun+4][colRun+4] = a0;  // B[4][4] = A[4][4] For example
+          B[rowRun+4][colRun+0] = B[rowRun+3][colRun+4];
+          B[rowRun+4][colRun+4] = a0; 
           B[rowRun+3][colRun+4] = a4;
           B[rowRun+4][colRun+1] = B[rowRun+3][colRun+5];
           B[rowRun+4][colRun+5] = a1;
@@ -126,9 +111,8 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
           B[rowRun+4][colRun+7] = a3;
           B[rowRun+3][colRun+7] = a7;
 
-          // this loops deal with the the remaning elements .
+          // This loops deal with the other elements that remain.
           for(k=0;k<3;k++){
-
             a0 = A[colRun+4][rowRun+5+k];
             a1 = A[colRun+5][rowRun+5+k];
             a2 = A[colRun+6][rowRun+5+k];
@@ -159,17 +143,12 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
     b_s = 16;
     
     for (j_block = 0; j_block < M; j_block += b_s){
-      for (i_block = 0; i_block < N; i_block += b_s){ 
-        /*not all blocks will be square, so (rowBlock + 16 > N) may get an invalid access. Therefore we need to regularly check for r<N and c<M */
+      for (i_block = 0; i_block < N; i_block += b_s){
         for(i = i_block; (i < N) && (i < i_block + b_s); i++){
           for(j = j_block; (j < M) && (j < j_block + b_s); j++){
-            //row and column are not same
-            if (i != j){
+            if (i != j){ //if i and j not same (rows and collums)
               B[j][i] = A[i][j];
-            }
-            
-            //row and column same. process like 32 x 32 
-            else{
+            else{ //if i and j the same (rows and collums)
               temp = A[i][j];
               d = i;
             }
